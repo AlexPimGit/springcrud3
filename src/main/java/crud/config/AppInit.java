05efb3b16fe079530267c1c6396bba8997a6aaac
Alex Pim <shurik__@tut.by>
2020-02-20 19:58:46 +0300

    v1.4

--- src/main/java/crud/config/AppInit.java
+++ src/main/java/crud/config/AppInit.java
@@ -1,6 +1,19 @@
 package crud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import javax.servlet.SessionTrackingMode;
+import java.util.EnumSet;
+import java.util.Set;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -25,6 +38,53 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void registerDispatcherServlet(ServletContext servletContext) {
+        String servletName = getServletName();
+        Assert.hasLength(servletName, "getServletName() must not return empty or null");
+
+        WebApplicationContext servletAppContext = createServletApplicationContext();
+        Assert.notNull(servletAppContext,
+                "createServletApplicationContext() did not return an application " +
+                        "context for servlet [" + servletName + "]");
+
+        FrameworkServlet dispatcherServlet = createDispatcherServlet(servletAppContext);
+        dispatcherServlet.setContextInitializers(getServletApplicationContextInitializers());
+
+        ServletRegistration.Dynamic registration = servletContext.addServlet(servletName, dispatcherServlet);
+        Assert.notNull(registration,
+                "Failed to register servlet with name '" + servletName + "'." +
+                        "Check if there is another servlet registered under the same name.");
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping(getServletMappings());
+        registration.setAsyncSupported(isAsyncSupported());
+//добавил
+        servletContext.setSessionTrackingModes(getSessionTrackingModes());
+
+        Filter[] filters = getServletFilters();
+        if (!ObjectUtils.isEmpty(filters)) {
+            for (Filter filter : filters) {
+                registerServletFilter(servletContext, filter);
+            }
+        }
+
+        customizeRegistration(registration);
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};
+    }
+
+
+    protected Set<SessionTrackingMode> getSessionTrackingModes() {
+        return EnumSet.of(SessionTrackingMode.COOKIE);
     }
 
 }
--- src/main/webapp/WEB-INF/pages/users.jsp
+++ src/main/webapp/WEB-INF/pages/users.jsp
@@ -1,8 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
-<%@ taglib prefix="from" uri="http://www.springframework.org/tags/form" %>
-<%@ page session="false" %>
 <html>
 <head>
     <title>Users Page</title>
